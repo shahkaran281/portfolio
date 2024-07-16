6eff7296bcb6287d201a84123074de85d3a3fcf0
shahkaran281 <shahkaran281@gmail.com>
2024-07-16 13:55:58 -0700

    Added Navbar

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
       <main className="bg-white px-10 ">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">Karan Manishkumar Shah</h1>
+            <h1 className="text-xl font-burtons">Karan Manishkumar Shah</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
