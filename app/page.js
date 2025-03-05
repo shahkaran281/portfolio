42bc6aa839d398fbbb824a09581eb5e9c5fdc89c
shahkaran281 <shahkaran281@gmail.com>
2025-03-05 11:11:34 -0800

    Updated Resume

--- app/page.js
+++ app/page.js
@@ -34,7 +34,7 @@ export default function Home() {
               <li>
                 <a
                   className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="https://drive.google.com/file/d/1eKTR8WQEmFZzQRVxhUZmgm3u9SOiTKBl/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1G2h6qmQhscg2400SM1uvB1EMFzB_E4Wr/view?usp=sharing"
                   target="_blank"
                 >
                   Resume
